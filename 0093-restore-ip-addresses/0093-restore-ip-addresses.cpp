67f26099aefbf8114aa896f24d23c9dbc980dc22
SonuKumarSingh <sonukumarsingh.5087@gmail.com>
2026-08-10 21:29:10 +0530

    Time: 0 ms (100%), Space: 10.8 MB (31.41%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void solve(string &s, int index, int parts, string curr) {
+
+        // We need exactly 4 parts
+        if(parts == 4) {
+            if(index == s.size()) {
+                curr.pop_back(); // remove last '.'
+                ans.push_back(curr);
+            }
+            return;
+        }
+
+        // Try taking 1, 2, or 3 digits
+        for(int len = 1; len <= 3; len++) {
+
+            // Not enough characters left
+            if(index + len > s.size())
+                break;
+
+            string part = s.substr(index, len);
+
+            // Leading zero is invalid
+            if(part.size() > 1 && part[0] == '0')
+                continue;
+
+            // Value must be <= 255
+            if(stoi(part) > 255)
+                continue;
+
+            solve(
+                s,
+                index + len,
+                parts + 1,
+                curr + part + "."
+            );
+        }
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+
+        // An IP has 4 parts, each 1-3 digits
+        if(s.size() < 4 || s.size() > 12)
+            return {};
+
+        solve(s, 0, 0, "");
+
+        return ans;
+    }
+};
